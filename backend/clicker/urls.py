54e91dfbce36c70fcb7f37a4e496eb05b2d7cddf
Klymenko Illia <sovuh2703@gmail.com>
2023-10-18 20:35:15 +0300

    add api to admin url

--- backend/clicker/urls.py
+++ backend/clicker/urls.py
@@ -6,7 +6,7 @@ from django.views.static import serve
 
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('api/admin/', admin.site.urls),
     re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
     re_path(r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT}),
 ]
