c6148b43dd725fdafa3efdd8429bbb6d53642943
Andrii Sachok <andriy.sachok@gmail.com>
2023-10-23 14:05:13 +0300

    feat: add httpRequests
    commit 6653311100799395c2c9dd87645cbe6f0d178df6
    Author: Andrii Sachok <andriy.sachok@gmail.com>
    Date:   Mon Oct 23 14:02:41 2023 +0300
    
        feat: add getUser request
    
    commit bde23e88460afb96fe63462bd0e2da7a54735fa8
    Author: Andrii Sachok <andriy.sachok@gmail.com>
    Date:   Mon Oct 23 13:57:34 2023 +0300
    
        feat: add resetPassword requests
    
    commit 88e0e33c93e599f109395c72c4fa87d771e4b1b9
    Author: Andrii Sachok <andriy.sachok@gmail.com>
    Date:   Mon Oct 23 13:45:50 2023 +0300
    
        feat: add refreshAccessToken request
    
    commit 7e02e113631fb8d92198ff51e90b690e457429b3
    Author: Andrii Sachok <andriy.sachok@gmail.com>
    Date:   Mon Oct 23 13:43:55 2023 +0300
    
        feat: add checkAuth request
    
    commit 511e62983f8226b6cd2c07d79a546d439aa71743
    Author: Andrii Sachok <andriy.sachok@gmail.com>
    Date:   Mon Oct 23 13:42:12 2023 +0300
    
        feat: add getAccessToken request
    
    commit 27173ca0473c63909bc77466d23f5652d39a238b
    Author: Andrii Sachok <andriy.sachok@gmail.com>
    Date:   Mon Oct 23 13:38:25 2023 +0300
    
        feat: add activateAccount request
    
        Also renamed httpRequsets.js to httpRequest.js
    
    commit 452f23be69c0df891a42c4d16f9e2340ca0cd7a1
    Author: Andrii Sachok <andriy.sachok@gmail.com>
    Date:   Mon Oct 23 13:28:58 2023 +0300
    
        feat: add createUser request

---
+++ frontend/src/functions/httpRequests.js
@@ -0,0 +1,124 @@
+const HOST = 'http://127.0.0.1:8000';
+
+export const createUser = async () => {
+  const response = await fetch(`${HOST}/api/auth/users/`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      Accept: 'application/json',
+    },
+    body: {
+      email: 'user.email@example.com',
+      name: 'User Name',
+      password: 'password',
+      re_password: 'password',
+    },
+  });
+
+  return response.json();
+};
+
+export const activateAccount = async () => {
+  const response = await fetch(`${HOST}/api/auth/users/activation/`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: {
+      uid: 'NQ',
+      token: 'activation_token',
+    },
+  });
+
+  return response.json();
+};
+
+export const getAccessToken = async () => {
+  const response = await fetch(`${HOST}/api/auth/jwt/create/`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: {
+      email: 'user.email@example.com',
+      password: 'password',
+    },
+  });
+
+  return response.json();
+};
+
+export const checkAuth = async () => {
+  const response = await fetch(`${HOST}/api/auth/jwt/verify/`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: {
+      token: 'access_token',
+    },
+  });
+
+  return response.json();
+};
+
+export const refreshAccessToken = async () => {
+  const response = await fetch(`${HOST}/api/auth/jwt/refresh/`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: {
+      token: 'access_token',
+    },
+  });
+
+  return response.json();
+};
+
+export const resetPasswordMail = async () => {
+  const response = await fetch(`${HOST}/api/auth/users/reset_password/`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: {
+      email: 'user.email@example.com',
+    },
+  });
+
+  return response.json();
+};
+
+export const resetPasswordConfirm = async () => {
+  const response = await fetch(
+    `${HOST}/api/auth/users/reset_password_confirm/`,
+    {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: {
+        uid: 'Mg',
+        token: 'reset_password_token',
+        new_password: 'password',
+        re_new_password: 'password',
+      },
+    }
+  );
+
+  return response.json();
+};
+
+export const getUser = async () => {
+  const response = await fetch(`${HOST}/api/auth/users/me/`, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: 'JWT access_token',
+      Accept: 'application/json',
+    },
+  });
+
+  return response.json();
+};
