69e54f79e6aef6cd7f990c967c874b9a816337ce
Andrii Sachok <andriy.sachok@gmail.com>
2023-10-31 20:30:03 +0200

    feat: update footer layout

--- frontend/src/components/UI/Footer.jsx
+++ frontend/src/components/UI/Footer.jsx
@@ -1,17 +1,21 @@
-import { AppBar, Typography } from '@mui/material';
+import { AppBar, Stack, Typography, IconButton } from '@mui/material';
+import GitHub from '@mui/icons-material/GitHub';
 
 const Footer = () => {
   return (
     <AppBar
       position="static"
       sx={{
-        display: 'flex',
         justifyContent: 'center',
-        alignItems: 'center',
         height: '50px',
       }}
     >
-      <Typography variant="h6">Footer</Typography>
+      <Stack direction="row" justifyContent="center" alignItems="center">
+        <Typography variant="h6">© Clicker App, 2023</Typography>
+        <IconButton color="inherit" href="https://github.com/soovuh/clicker">
+          <GitHub />
+        </IconButton>
+      </Stack>
     </AppBar>
   );
 };
