62a0f6a1718478e15015c76b74ab09cdd7d2c6e1
Andrii Sachok <andriy.sachok@gmail.com>
2023-10-31 20:51:39 +0200

    feat: add "Reset password" option to login form

--- frontend/src/components/pages/auth/Login.jsx
+++ frontend/src/components/pages/auth/Login.jsx
@@ -1,12 +1,30 @@
 import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { Paper, Stack, Typography, TextField, Button } from '@mui/material';
+import {
+  Paper,
+  Stack,
+  Typography,
+  TextField,
+  Breadcrumbs,
+  Button,
+} from '@mui/material';
 import { loginUser } from '../../../functions/httpRequests';
 import { useAuth } from '../../../context/AuthContext';
 import RouterLink from '../../UI/RouterLink';
 import Divider from '../../UI/Divider';
 import GoogleAuthButton from '../../UI/GoogleAuthButton';
 import GitHubAuthButton from '../../UI/GitHubAuthButton';
+
+const CustomTypography = ({ children }) => {
+  return (
+    <Typography
+      variant="body2"
+      sx={{ transition: '0.1s', '&:hover': { color: '#1976d2' } }}
+    >
+      {children}
+    </Typography>
+  );
+};
 
 const Login = () => {
   const [user, setUser] = useState({ email: '', password: '' });
@@ -76,14 +94,14 @@ const Login = () => {
           value={user.password}
           onChange={handlePasswordChange}
         />
-        <RouterLink to="/signup">
-          <Typography
-            variant="body2"
-            sx={{ transition: '0.1s', '&:hover': { color: '#1976d2' } }}
-          >
-            Don't have an account yet?
-          </Typography>
-        </RouterLink>
+        <Breadcrumbs>
+          <RouterLink to="/signup">
+            <CustomTypography>Sign up</CustomTypography>
+          </RouterLink>
+          <RouterLink to="/reset-password">
+            <CustomTypography>Reset password</CustomTypography>
+          </RouterLink>
+        </Breadcrumbs>
         <Button
           variant="contained"
           type="submit"
