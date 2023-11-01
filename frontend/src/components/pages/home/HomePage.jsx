b6094287ea2ac2430c79ab674481229b0ee4353f
Andrii Sachok <andriy.sachok@gmail.com>
2023-11-01 22:45:49 +0200

    feat: connect user from DB to home page

--- frontend/src/components/pages/home/HomePage.jsx
+++ frontend/src/components/pages/home/HomePage.jsx
@@ -1,6 +1,8 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import BigButton from './BigButton';
 import Counter from './Counter';
+import { useAuth } from '../../../context/AuthContext';
+import { getUser } from '../../../functions/httpRequests';
 
 import { Stack } from '@mui/material';
 import Display from '../../UI/Display';
@@ -13,12 +15,31 @@ const layout = {
   alignItems: 'center',
 };
 
+const INITIAL_USER = { email: '', username: 'Unknown', id: '' };
+
 const HomePage = () => {
+  const { accessToken } = useAuth();
+  const [user, setUser] = useState(INITIAL_USER);
   const [counter, setCounter] = useState(0);
+
+  useEffect(() => {
+    const fetchUser = async () => {
+      const response = await getUser(accessToken);
+
+      if (response.code) {
+        return;
+      }
+
+      console.log('response', response);
+      setUser(response);
+    };
+    fetchUser();
+  }, [accessToken]);
+
   return (
     <Stack sx={layout}>
       <Stack direction="column" spacing={2} alignItems="center">
-        <Display value="Username" sx={{ color: 'white', variant: 'h5' }} />
+        <Display value={user.username} sx={{ color: 'white', variant: 'h5' }} />
         <Counter counter={counter} />
         <BigButton setCounter={setCounter} />
       </Stack>
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -52,11 +52,11 @@ const router = createBrowserRouter([
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <AuthProvider>
-      <RouterProvider router={router} />
-    </AuthProvider>
-  </React.StrictMode>
+  // <React.StrictMode>
+  <AuthProvider>
+    <RouterProvider router={router} />
+  </AuthProvider>
+  // </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
