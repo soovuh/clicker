4593fae71e9b1a2c41de4ec1e74496e3037938d6
Andrii Sachok <andriy.sachok@gmail.com>
2024-01-15 16:19:38 +0200

    feat(undone)!: add profile page layout

--- frontend/src/components/pages/profile/ProfilePage.jsx
+++ frontend/src/components/pages/profile/ProfilePage.jsx
@@ -1,6 +1,6 @@
 import { useAuth } from '../../../context/AuthContext';
 import { useUserData } from '../../../hooks/useUserData';
-import { Stack, Typography } from '@mui/material';
+import { Stack, Box, Typography, Paper } from '@mui/material';
 import Loading from '../../UI/Loading';
 
 const ProfilePage = () => {
@@ -17,12 +17,46 @@ const ProfilePage = () => {
       {loading ? (
         <Loading type="bars" />
       ) : (
-        <Stack>
-          <Typography>ImageURL: {image}</Typography>
-          <Typography>Username: {username}</Typography>
-          <Typography>Email: {email}</Typography>
-          <Typography>Clicks: {clicks}</Typography>
-        </Stack>
+        <Paper
+          elevation={4}
+          sx={{
+            padding: {
+              xs: '0.5rem',
+              sm: '1rem',
+            },
+            borderRadius: '1rem',
+            overflow: 'hidden',
+          }}
+        >
+          <Box
+            sx={{
+              width: {
+                xs: '240px',
+                sm: '300px',
+              },
+              height: {
+                xs: '240px',
+                sm: '300px',
+              },
+              borderRadius: '1rem',
+              overflow: 'hidden',
+            }}
+          >
+            <img
+              src={image}
+              alt={username}
+              style={{
+                width: 'inherit',
+                height: 'inherit',
+              }}
+            />
+          </Box>
+          <Stack>
+            <Typography>Username: {username}</Typography>
+            <Typography>Email: {email}</Typography>
+            <Typography>Clicks: {clicks}</Typography>
+          </Stack>
+        </Paper>
       )}
     </Stack>
   );
